95dcda599679a49ffbe6c63fa8e1b9169bbe4d1e
sarayu2005 <valtetisarayu@gmail.com>
2026-03-26 15:02:03 +0530

    created Thickshakes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Categories from './components/Categories/Categories'
+import Thickshakes from './components/Thickshakes/Thickshakes'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Categories />
+      <Thickshakes />
     </main>
   )
 }
